29e10d0b615d2bbb5059a9140b0ef0ce5892f567
IevgenLychman <gora.erikovich@gmail.com>
2022-10-26 23:26:41 +0300

    1

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+// const categoriesNames = document.querySelector("h2").textContent;
+// console.log("Category:  ", categoriesNames);
+
+const categoriesQty = document.querySelector("#categories").children.length;
+console.log("Number of categories: ", categoriesQty);
+
+// const two = document.querySelectorAll("li h2").textContent;
+// console.log(two);
+
+const three = document.querySelector(".item").lastElementChild;
+console.log(three);
+
+// const what = document.querySelector(".item");
+
+// const what = document.querySelector("#categories");
+// console.log(what.children);
+
+// console.log(what.children[0].children[0].textContent);
+// console.log(what.children[0].children[1].length);
