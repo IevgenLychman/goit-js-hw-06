d43b39df36b9d884a8428bf871e796be56120c8f
IevgenLychman <gora.erikovich@gmail.com>
2022-10-27 23:00:14 +0300

    3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,24 +1,13 @@
-// const categoriesQty = document.querySelector("#categories").children.length;
-// console.log("Number of categories: ", categoriesQty);
+const categoriesList = [];
 
-// const categoriesNames = document.querySelector("h2").textContent;
-// console.log("Category:  ", categoriesNames);
+const categoriesLength = document.querySelector("#categories").children.length;
+categoriesList.push(`Number of categories: ${categoriesLength}`);
 
-const categoriesFirstLi = Array.from(
-  document.querySelector("#categories").children
-);
-
-const categoriesUl = Array.from(
-  document.querySelectorAll("#categories  li ul")
-);
-
-const categoriesList = [{}];
-
-categoriesFirstLi.forEach((element) => {
+const categoriesFilling = document.querySelectorAll(".item");
+categoriesFilling.forEach((element) => {
   categoriesList.push(
-    `Number of categories: ${categoriesFirstLi.length}`,
-    `Category: ${element.firstChild.textContent.trim()}`,
-    `Elements: ${element.firstElementChild.children.length}`
+    `Category: ${element.firstElementChild.textContent}`,
+    `Elements: ${element.lastElementChild.children.length}`
   );
 });
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ind = document.querySelector("#ingredients");
+
+const addIngredients = ingredients.map((ingredient) => {
+  const ingredientsList = document.createElement("li");
+  ingredientsList.textContent = ingredient;
+  ingredientsList.classList.add("item");
+  return ingredientsList;
+});
+
+ind.append(...addIngredients);
+
+console.log(addIngredients);
