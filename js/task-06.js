07bd816d1fd8d0e940d3d3dd03eba6078f4ae256
IevgenLychman <gora.erikovich@gmail.com>
2022-11-03 13:36:43 +0200

    Final

--- js/task-06.js
+++ js/task-06.js
@@ -2,20 +2,15 @@ const refs = {
   inputField: document.querySelector("#validation-input"),
   inputLength: document.querySelector("input").getAttribute("data-length"),
 };
-function fieldValid() {
-  refs.inputField.classList.add("valid");
-  refs.inputField.classList.remove("invalid");
-}
-
-function fieldInvalid() {
-  refs.inputField.classList.remove("valid");
-  refs.inputField.classList.add("invalid");
+function fieldValidation(add, remove) {
+  refs.inputField.classList.add(add);
+  refs.inputField.classList.remove(remove);
 }
 
 const inputCheckContent = () => {
   refs.inputField.value.length === Number(refs.inputLength)
-    ? fieldValid()
-    : fieldInvalid();
+    ? fieldValidation("valid", "invalid")
+    : fieldValidation("invalid", "valid");
 };
 
 refs.inputField.addEventListener("blur", inputCheckContent);
